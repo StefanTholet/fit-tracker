601039971f45a010cbac62fd7d3a3459b45c0f8f
StefanTholet <stefan.tholet@gmail.com>
2024-05-20 22:43:00 +0300

    progress sql setup

---
+++ lib/data.ts
@@ -0,0 +1,10 @@
+// import { db } from '@vercel/postgres'
+
+// export default async function handler(request, response) => {
+//   const client = await db.connect()
+//   try {
+//     await client.sql(`CREATE TABLE Pets (Name varchar(255))`)
+//   } catch (error) {
+
+//   }
+// }
