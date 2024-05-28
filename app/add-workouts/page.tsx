30fb24bcd7dc8b7025aff616ff817fafbbe0597c
StefanTholet <stefan.tholet@gmail.com>
2024-05-28 21:56:37 +0300

    begin route guard logic

--- app/add-workouts/page.tsx
+++ app/add-workouts/page.tsx
@@ -1,5 +1,12 @@
+import { getSession } from '@/actions/auth-actions'
 import AddWorkouts from '@/components/add-workouts'
-const AddWorkout = () => {
+import { redirect } from 'next/navigation'
+const AddWorkout = async () => {
+  const session = await getSession()
+  // const { isLoggedIn, userId } = session
+  if (!session.isLoggedIn) {
+    redirect('/login')
+  }
   return (
     <div className="w-10/12 m-auto flex flex-col">
       <h1 className="text-3xl font-bold mb-4 mx-auto">Add your workouts</h1>
