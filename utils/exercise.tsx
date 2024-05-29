2f45fe5545e5ff21f9573da64e7b8ee784c98e10
StefanTholet <stefan.tholet@dxc.com>
2024-05-29 12:11:25 +0300

    fix exercise validation

--- utils/exercise.tsx
+++ utils/exercise.tsx
@@ -1,5 +1,5 @@
 import { Exercise } from '@/interfaces/exercise'
 
 export const isExerciseValid = (exercise: Exercise) => {
-  return exercise.name && exercise.reps && exercise.sets
+  return exercise.name && exercise.sets[0].weight && exercise.sets[0].reps
 }
